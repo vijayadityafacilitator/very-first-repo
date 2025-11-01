56b30ea3fc10e2f34fc5f33bfc4134752a526a2f
vijayadityafacilitator <vijayadityafacilitator@gmail.com>
2025-11-01 13:38:50 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class factorial {
+    Scanner sc = new Scanner(System.in);
     
+    int fact = 1;
+
+    System.out.println("Enter a number: ");
+    int num = sc.nextInt();
+
+    for(int i = 1; i < num; i++){
+        fact = fact * i;
+    }
+    System.out.println(fact);
 }
